e7c73a1888bb50cd43616da05be4bdc8c774e714
Ollie Riches <ollie.riches@btinternet.com>
2015-08-26 14:47:44 +0100

    Update MainWindow.xaml.cs

--- Simple.Wpf.Exceptions/Views/MainWindow.xaml.cs
+++ Simple.Wpf.Exceptions/Views/MainWindow.xaml.cs
@@ -1,36 +1,27 @@
 using System;
+using System.Reactive;
 using System.Reactive.Linq;
 using System.Reactive.Threading.Tasks;
+using MahApps.Metro.Controls;
 using MahApps.Metro.Controls.Dialogs;
 using Simple.Wpf.Exceptions.Services;
-using Simple.Wpf.Exceptions.ViewModels;
 
 namespace Simple.Wpf.Exceptions.Views
 {
-    public partial class MainWindow
+    public partial class MainWindow : MetroWindow
     {
         private readonly IDisposable _disposable;
 
         public MainWindow(IMessageService messageService, ISchedulerService schedulerService)
         {
             InitializeComponent();
-
-            var settings = new MetroDialogSettings
-            {
-                AnimateShow = true,
-                AnimateHide = true,
-                ColorScheme = MetroDialogColorScheme.Inverted
-            };
-
+            
             _disposable = messageService.Show
                 // Delay to make sure there is time for the animations
                 .Delay(TimeSpan.FromMilliseconds(250), schedulerService.TaskPool)
                 .ObserveOn(schedulerService.Dispatcher)
-                .Select(x => new MessageDialog { Title = x.Header, Content = x.ViewModel })
-                .SelectMany(x => this.ShowMetroDialogAsync(x, settings).ToObservable(), (x, y) => x)
-                .ObserveOn(schedulerService.Dispatcher)
-                .SelectMany(x => ((CloseableViewModel)x.Content).Closed, (x, y) => x)
-                .Select(x => this.HideMetroDialogAsync(x).ToObservable())
+                .Select(x => new MessageDialog(x))
+                .SelectMany(ShowDialogAsync, (x, y) => x)
                 .Subscribe();
 
             Closed += HandleClosed;
@@ -39,6 +30,21 @@ namespace Simple.Wpf.Exceptions.Views
         private void HandleClosed(object sender, EventArgs e)
         {
             _disposable.Dispose();
+        }
+        
+        private IObservable<Unit> ShowDialogAsync(MessageDialog dialog)
+        {
+            var settings = new MetroDialogSettings
+            {
+                AnimateShow = true,
+                AnimateHide = true,
+                ColorScheme = MetroDialogColorScheme.Inverted
+            };
+
+            return this.ShowMetroDialogAsync(dialog, settings)
+                .ToObservable()
+                .SelectMany(x => dialog.CloseableContent.Closed, (x, y) => x)
+                .SelectMany(x => this.HideMetroDialogAsync(dialog).ToObservable(), (x, y) => x);
         }
     }
 }

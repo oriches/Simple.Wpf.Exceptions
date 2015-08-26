4daa398c53ac83108c7b0c8b63debd0df05f9234
Ollie Riches <ollie.riches@btinternet.com>
2015-08-26 14:47:58 +0100

    Update MessageDialog.cs

--- Simple.Wpf.Exceptions/Views/MessageDialog.cs
+++ Simple.Wpf.Exceptions/Views/MessageDialog.cs
@@ -1,10 +1,22 @@
 using System.Windows.Markup;
 using MahApps.Metro.Controls.Dialogs;
+using Simple.Wpf.Exceptions.ViewModels;
 
 namespace Simple.Wpf.Exceptions.Views
 {
     [ContentProperty("DialogBody")]
     public sealed class MessageDialog : BaseMetroDialog
     {
+        private readonly MessageViewModel _viewModel;
+
+        public MessageDialog(MessageViewModel viewModel)
+        {
+            _viewModel = viewModel;
+
+            Title = _viewModel.Header;
+            Content = _viewModel.ViewModel;
+        }
+
+        public CloseableViewModel CloseableContent { get { return _viewModel.ViewModel; } }
     }
 }

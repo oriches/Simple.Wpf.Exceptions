1ecaabe53aaaed72ca8331b74b2d89ef1bc0b72b
Ollie Riches <ollie.riches@btinternet.com>
2015-08-26 14:59:38 +0100

    Update App.xaml.cs

--- Simple.Wpf.Exceptions/App.xaml.cs
+++ Simple.Wpf.Exceptions/App.xaml.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Diagnostics;
 using System.Reactive.Disposables;
+using System.Reactive.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media;

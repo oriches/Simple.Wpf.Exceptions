8053de4f87ce27fbb45d5fb205814ceb02010f1f
Ollie Riches <ollie.riches@btinternet.com>
2015-08-26 14:47:00 +0100

    Update MessageService.cs

--- Simple.Wpf.Exceptions/Services/MessageService.cs
+++ Simple.Wpf.Exceptions/Services/MessageService.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
@@ -11,17 +12,16 @@ namespace Simple.Wpf.Exceptions.Services
 {
     public sealed class MessageService : IMessageService, IDisposable
     {
-        private readonly ISchedulerService _schedulerService;
         private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
         private readonly IDisposable _disposable;
         private readonly Subject<MessageViewModel> _show;
         private readonly Queue<MessageViewModel> _waitingMessages = new Queue<MessageViewModel>();
-	private readonly object _sync = new object();
+		
+		private readonly object _sync = new object();
 
-        public MessageService(ISchedulerService schedulerService)
+        public MessageService()
         {
-            _schedulerService = schedulerService;
             _show = new Subject<MessageViewModel>();
 
             _disposable = Disposable.Create(() =>
@@ -41,44 +41,44 @@ namespace Simple.Wpf.Exceptions.Services
             }
         }
 
-        public void Post(string header, CloseableViewModel viewModel, IDisposable lifetime)
+        public IObservable<Unit> Post(string header, CloseableViewModel viewModel, IDisposable lifetime)
         {
             var newMessage = new MessageViewModel(header, viewModel, lifetime);
 			
             newMessage.ViewModel.Closed
-		.Take(1)
-                .Subscribe(x =>
+				.Take(1)
+			    .Subscribe(x =>
                 {
-		   MessageViewModel nextMessage = null;
-		   lock(_sync)
-		   {
-		      _waitingMessages.Dequeue();
-		
-		   if (_waitingMessages.Any())
-		   {
-		      nextMessage = _waitingMessages.Peek();
-		   }
-		}
-		
-		if (nextMessage != null)
-		{
-		   _show.OnNext(nextMessage);
-		}
-				
-		newMessage.ViewModel.Dispose();
+					MessageViewModel nextMessage = null;
+					lock(_sync)
+					{
+						_waitingMessages.Dequeue();
+						
+						if (_waitingMessages.Any())
+						{
+							nextMessage = _waitingMessages.Peek();
+						}
+					}
+					
+					if (nextMessage != null)
+					{
+						_show.OnNext(nextMessage);
+					}
                 });
 
-	   var show = false;
-	   lock(_sync)
-	   {
-		_waitingMessages.Enqueue(newMessage);
-		show = _waitingMessages.Count == 1;
-	   }
+			bool show;
+			lock(_sync)
+			{
+				_waitingMessages.Enqueue(newMessage);
+				show = _waitingMessages.Count == 1;
+			}
             
             if (show)
             {
                 _show.OnNext(newMessage);
             }
+
+            return viewModel.Closed;
         }
 
         public IObservable<MessageViewModel> Show { get { return _show; } }
